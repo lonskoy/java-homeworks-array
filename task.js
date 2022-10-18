979799e0882558133ce36d2c1fc2b1f58cd31e69
Lonskoy <lonskoy0304@gmail.com>
2022-10-18 23:25:05 +0300

    edit

--- task.js
+++ task.js
@@ -1,11 +1,6 @@
 function compareArrays(arr1, arr2) {
-
-  const result = (arr1, arr2) => {
-    if (arr1.length === arr2.length && arr1.every(item, index) => item === arr2[index]) 
-    {return true}
-    return false;
-  }
-  return result; // boolean
+  
+  return (arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])) ; 
 }
 
 function advancedFilter(arr) {
